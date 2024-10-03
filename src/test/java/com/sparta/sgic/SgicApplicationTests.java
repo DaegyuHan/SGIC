995469a014ed1cf3c789c55de7b9d3020e1cd31e
Daegyu Han <bigstar77han@gmail.com>
2024-10-04 00:34:49 +0900

    SGIC init

---
+++ src/test/java/com/sparta/sgic/SgicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.sgic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
